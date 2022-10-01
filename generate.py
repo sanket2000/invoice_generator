2f86c3da84c2fc4f55f57e79711af383a3135394
sanketrasik <sanketrasik@gmail.com>
2022-10-01 12:35:02 +0530

    helpful messages inserted

--- generate.py
+++ generate.py
@@ -115,4 +115,18 @@ def main():
             
 
 if __name__ == "__main__":
+    print("""
+Hello,
+
+Note: This program assumes you have python3.6 or later
+
+With this program you can generate pdf files from .csv data and .odt template file.
+
+Step1: Create a formated .odt file as template.
+Step2: Put place-holder values in template file to be replaced.
+Step3: Create a .csv file with place-holder vales as headers.
+Step4: Put replacement value under header rows.
+Step5: Place .csv and .odt file into 'data' folder
+
+    """)
     main()
